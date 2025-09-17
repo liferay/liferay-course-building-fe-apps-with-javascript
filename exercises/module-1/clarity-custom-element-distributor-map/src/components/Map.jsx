799f658a3d659af785de4a3f06fbce4230f6b382
Ricky Pan <ricky.pan@liferay.com>
2025-09-16 17:57:18 -0700

    Update exercise files to reduce redundancy

--- exercises/module-1/clarity-custom-element-distributor-map/src/components/Map.jsx
+++ exercises/module-1/clarity-custom-element-distributor-map/src/components/Map.jsx
@@ -7,10 +7,10 @@ const mapContainerStyle = {
 };
 
 const locations = [
-    { id: 1, name: 'Warby Parker', street: '11700 Domain Blvd Suite 114', city: 'Austin', state: 'TX', zipCode: '78758', position: {lat: 30.402968, lng: -97.720991} },
-    { id: 2, name: 'ClearView Optics', street: '1980 East State Hwy 114', city: 'Southlake', state: 'TX', zipCode: '76092', position: {lat: 32.945113, lng: -97.121117} },
-    { id: 3, name: 'Stanton Optical', street: '3620 W University Dr #400', city: 'McKinney', state: 'TX', zipCode: '75071', position: {lat: 33.217463, lng: -96.660282} },
-    { id: 3, name: 'Roka', street: '5646 Milton St. Suite 540', city: 'Dallas', state: 'TX', zipCode: '75206', position: {lat: 32.848092, lng: -96.770822} }
+    { id: 1, name: 'Warby Parker', street: '11700 Domain Blvd Suite 114', city: 'Austin', state: 'TX', zipCode: '78758', tier: {key: 'gold', name: 'Gold'}, position: {lat: 30.402968, lng: -97.720991} },
+    { id: 2, name: 'ClearView Optics', street: '1980 East State Hwy 114', city: 'Southlake', state: 'TX', zipCode: '76092', tier: {key: 'bronze', name: 'Bronze'}, position: {lat: 32.945113, lng: -97.121117} },
+    { id: 3, name: 'Stanton Optical', street: '3620 W University Dr #400', city: 'McKinney', state: 'TX', zipCode: '75071', tier: {key: '', name: ''}, position: {lat: 33.217463, lng: -96.660282} },
+    { id: 4, name: 'Roka', street: '5646 Milton St. Suite 540', city: 'Dallas', state: 'TX', zipCode: '75206', tier: {key: 'silver', name: 'Silver'}, position: {lat: 32.848092, lng: -96.770822} }
 ];
 
 // Center on the first location
--- exercises/module-1/clarity-custom-element-distributor-table/src/components/DistributorTable.jsx
+++ exercises/module-1/clarity-custom-element-distributor-table/src/components/DistributorTable.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import ClayTable from '@clayui/table';
 import ClayButton from '@clayui/button';
+
 const DistributorTable = () => {
     const [distributors, setDistributors] = useState([]);
 
     useEffect(() => {
         setDistributors([
-            { id: 1, name: 'Vision World', city: 'Austin', state: 'TX' },
-            { id: 2, name: 'ClearView Optics', city: 'Dallas', state: 'TX' },
-            { id: 3, name: 'FocusPlus', city: 'Houston', state: 'TX' }
+            { id: 1, name: 'Warby Parker', street: '11700 Domain Blvd Suite 114', city: 'Austin', state: 'TX', zipCode: '78758', tier: {key: 'gold', name: 'Gold'}, position: {lat: 30.402968, lng: -97.720991} },
+            { id: 2, name: 'ClearView Optics', street: '1980 East State Hwy 114', city: 'Southlake', state: 'TX', zipCode: '76092', tier: {key: 'bronze', name: 'Bronze'}, position: {lat: 32.945113, lng: -97.121117} },
+            { id: 3, name: 'Stanton Optical', street: '3620 W University Dr #400', city: 'McKinney', state: 'TX', zipCode: '75071', tier: {key: '', name: ''}, position: {lat: 33.217463, lng: -96.660282} },
+            { id: 4, name: 'Roka', street: '5646 Milton St. Suite 540', city: 'Dallas', state: 'TX', zipCode: '75206', tier: {key: 'silver', name: 'Silver'}, position: {lat: 32.848092, lng: -96.770822} }
         ]);
     }, []);
 
